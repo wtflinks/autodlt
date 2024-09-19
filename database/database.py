ebe6e29f1817b69cf72d9b4d095077e33371000d
wtflinks <167952966+wtflinks@users.noreply.github.com>
2024-09-19 08:58:17 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,16 +1,8 @@
-#(©)CodeXBotz
-
-
-
-
 import pymongo, os
 from config import DB_URI, DB_NAME
 
-
 dbclient = pymongo.MongoClient(DB_URI)
 database = dbclient[DB_NAME]
-
-
 user_data = database['users']
 
 
